deac2a8b08711c35a011e1dc54233666da2ccea5
Daniel Tobon <daniel.tobon@uao.edu.co>
2022-03-05 07:08:42 -0500

    fixed second line
    it was added + third line in second line

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ Simple python hello world
 
 if __name__ == '__main__':
     print("-> first line")
-    print("-> second line")
+    print("-> second line + third")
     print("-> third line")
     print("-> four line")
 
